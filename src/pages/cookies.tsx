d9ed9035758a6aa512c3eb1b73d6d7b49cc9d13c
Jeffy Jeff <alexco4532@gmail.com>
2023-09-03 23:02:38 +0200

    fixing cookies declaration

--- src/pages/cookies.tsx
+++ src/pages/cookies.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Script from 'next/script';
 import Footer from '@/components/sections/footer';
 import Navbar from '@/components/sections/navbar';
 
 function Cookies() {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+
+        return () => {
+            setMounted(false);
+        }
+    }, []);
+
+    if (!mounted) return null;
     return (
         <div>
             <Navbar />
-            <Script id='CookieDeclaration' src="https://consent.cookiebot.com/65eac2d6-81a4-4a72-9272-9710402a045e/cd.js" type='text/javascript' async />
+            {mounted ? <>
+                <Script id='CookieDeclaration' src="https://consent.cookiebot.com/65eac2d6-81a4-4a72-9272-9710402a045e/cd.js" type='text/javascript' async />
+            </> : null}
         </div>
     )
 }
--- src/pages/prywatnosc.tsx
+++ src/pages/prywatnosc.tsx
@@ -1,9 +1,20 @@
 import Footer from '@/components/sections/footer';
 import Navbar from '@/components/sections/navbar';
 import Script from 'next/script';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function PrivacyPolicy() {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+
+        return () => {
+            setMounted(false);
+        }
+    }, []);
+
+    if (!mounted) return null;
     return (
         <div>
             <Navbar />
@@ -61,8 +72,9 @@ function PrivacyPolicy() {
                 <p>
                     9. Informacja o plikach cookies
                 </p>
-                <Script id='CookieDeclaration' src="https://consent.cookiebot.com/65eac2d6-81a4-4a72-9272-9710402a045e/cd.js" type='text/javascript' async />
-
+                {mounted ? <>
+                    <Script id='CookieDeclaration' src="https://consent.cookiebot.com/65eac2d6-81a4-4a72-9272-9710402a045e/cd.js" type='text/javascript' async />
+                </> : null}
             </main>
         </div>
     )
