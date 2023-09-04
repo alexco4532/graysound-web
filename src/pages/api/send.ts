d65cfc3df11dada5aa5024697377414aab2cb4a8
Jeffy Jeff <alexco4532@gmail.com>
2023-09-04 15:00:51 +0200

    update email message

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -13,31 +13,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const body: RequestBody = JSON.parse(req.body);
   try {
     const data = await resend.emails.send({
-      from: 'noreply <kontakt@graysoundlab.com>',
+      from: 'Graysound <noreply@graysoundlab.com>',
       to: [`${body.email}`],
       subject: `Formularz zgłoszeniowy - Graysound`,
-      text: `Z ogromnym entuzjazmem otrzymaliśmy Twoje zapytanie i chcielibyśmy serdecznie
-      podziękować za zainteresowanie naszymi usługami. Już teraz cieszymy się na myśl o
-      potencjalnej współpracy i jesteśmy pewni, że będziemy mogli spełnić Twoje
-      oczekiwania.
-      Twoje zapytanie zostało natychmiastowo przekazane do naszej reżyserki, która
-      odpowie na twoje wszystkie pytania. Martyna skontaktuje się z Tobą jak najszybciej,
-      aby omówić wszelkie szczegóły i przejść do kolejnych etapów realizacji.
-      Dziękujemy za wybór GraySound i nie możemy się doczekać, aby przekształcić Twoją
-      wizję w rzeczywistość.
-      Z serdecznymi pozdrowieniami,
+      text: `Z ogromnym entuzjazmem otrzymaliśmy Twoje zapytanie i chcielibyśmy serdecznie podziękować za zainteresowanie naszymi usługami. Już teraz cieszymy się na myśl o potencjalnej współpracy i jesteśmy pewni, że będziemy mogli spełnić Twoje oczekiwania.
+      
+      Twoje zapytanie zostało natychmiastowo przekazane do naszej reżyserki, która odpowie na twoje wszystkie pytania. Martyna skontaktuje się z Tobą jak najszybciej, aby omówić wszelkie szczegóły i przejść do kolejnych etapów realizacji. Dziękujemy za wybór GraySound i nie możemy się doczekać, aby przekształcić Twoją
+      wizję w rzeczywistość. Z serdecznymi pozdrowieniami,
       Zespół GraySound`
     });
 
     const notification = await resend.emails.send({
-      from: 'Form <kontakt@graysoundlab.com>',
+      from: 'Form <noreply@graysoundlab.com>',
       to: [
         'kontakt@graysoundlab.com',
         'valgardviggo@gmail.com',
         'j.samplawski@gmail.com',
-        'j.samplawski@graysoundlab.com'
+        'j.samplawski@graysoundlab.com',
+        'm.szymanska@graysoundlab.com'
       ],
-      subject: `Zgłoszenie - formularz od ${body.email}`,
+      subject: `Zapytanie od ${body.email}`,
       text: `Adres email: ${body.email}. Wiadomość: ${body.message}`
     });
 
