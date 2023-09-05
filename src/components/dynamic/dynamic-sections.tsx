7c4c51c2f68baff47f611d1110836d1139823379
Jeffy Jeff <alexco4532@gmail.com>
2023-09-05 18:04:17 +0200

    update dynamic imports

--- src/components/dynamic/dynamic-sections.tsx
+++ src/components/dynamic/dynamic-sections.tsx
@@ -3,11 +3,12 @@ import ClientsSection from '../sections/clients'
 import Listeners from '../sections/listeners'
 import FormSection from '../sections/form-section'
 import FooterSection from '../sections/footer'
+import SpatialSoundSection from '../sections/spatial-sound'
 
 function DynamicSections() {
     return (
         <>
-
+            <SpatialSoundSection />
             <ClientsSection />
             <Listeners />
             <FormSection />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ProsSection from "@/components/sections/pros";
 import dynamic from 'next/dynamic'
-import SpatialSoundSection from "@/components/sections/spatial-sound";
 
-//<script id="CookieDeclaration" src="https://consent.cookiebot.com/65eac2d6-81a4-4a72-9272-9710402a045e/cd.js" type="text/javascript" async></script>
 
 const DynamicSections = dynamic(() => import('../components/dynamic/dynamic-sections'), { ssr: false });
 const DynamicAudioPlayer = dynamic(() => import('../components/audio/audio-player'), { ssr: false });
@@ -25,7 +23,6 @@ export default function Home() {
           <HeaderSection />
           <DistributionSection />
           <ProsSection />
-          <SpatialSoundSection />
           <DynamicSections />
         </div>
       </main>
